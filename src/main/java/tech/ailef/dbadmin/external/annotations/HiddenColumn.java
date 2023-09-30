cd5076303d55933e4f35c61c78d3f4d27ce629ca
Francesco <stelk.mc@gmail.com>
2023-09-30 20:52:34 +0200

    Javadoc @HiddenColumn

--- src/main/java/tech/ailef/dbadmin/external/annotations/HiddenColumn.java
+++ src/main/java/tech/ailef/dbadmin/external/annotations/HiddenColumn.java
@@ -6,12 +6,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a column as hidden. This column and its values will not be shown
- * in the list and detail view for objects of this type. 
- * If the column is nullable, it will be hidden in the create and edit
- * forms as well (and this will result in the column having being NULL
- * when the objects are created/edited). If, instead, it's not a nullable
- * column, it will be included in the create and edit forms.
+ * <p>Marks a column as hidden. This column and its values will not be shown in the
+ * list and detail view for objects of this type. If the column is nullable, it
+ * will be hidden in the create and edit forms as well (and this will result 
+ * in the column always being NULL when creating/editing objects). If, instead,
+ * it's not nullable column, it will be included in the create and edit forms
+ * as it would otherwise prevent the creation of items.</p>
+ * 
+ * <p><strong>Please note that this is not meant as a security feature, </strong> but
+ * rather to hide uninformative columns that clutter the interface. In fact, since
+ * the create and edit form come pre-filled with all the information, these views
+ * <b>will</b> show the value of the hidden column (if it's not nullable).
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
