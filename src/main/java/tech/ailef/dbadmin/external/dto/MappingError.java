cc131739c34bfbfa0fb8d1d6e561c882444f9e07
Francesco <stelk.mc@gmail.com>
2023-10-05 13:15:51 +0200

    Minor changes

--- src/main/java/tech/ailef/dbadmin/external/dto/MappingError.java
+++ src/main/java/tech/ailef/dbadmin/external/dto/MappingError.java
@@ -1,5 +1,28 @@
+/*
+ * Spring Boot Database Admin - An automatically generated CRUD admin UI for Spring Boot apps
+ * Copyright (C) 2023 Ailef (http://ailef.tech)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package tech.ailef.dbadmin.external.dto;
 
+/**
+ * A wrapper class for information about mapping errors, i.e. errors that happen
+ * during the initialization phase, when table and fields are mapped to our internal
+ * representation.
+ */
 public class MappingError {
 	private String message;
 
--- src/main/java/tech/ailef/dbadmin/external/exceptions/UnsupportedFieldTypeException.java
+++ src/main/java/tech/ailef/dbadmin/external/exceptions/UnsupportedFieldTypeException.java
@@ -20,10 +20,8 @@
 package tech.ailef.dbadmin.external.exceptions;
 
 /**
- * Thrown during the computation of pagination if the requested
- * page number is not valid within the current request (e.g. it is greater
- * than the maximum available page). Used internally to redirect the
- * user to a default page. 
+ * Thrown when a field of an `@Entity` class has a type that is not
+ * supported.
  */
 public class UnsupportedFieldTypeException extends DbAdminException {
 	private static final long serialVersionUID = -8891734807568233099L;
