ec7eac3c12dd60b67d6cd4b095cc7fe28db70dde
Francesco <stelk.mc@gmail.com>
2023-10-20 15:03:26 +0200

    Edited templates to reference new resources routes (#19)

--- src/main/java/tech/ailef/dbadmin/external/MvcConfig.java
+++ src/main/java/tech/ailef/dbadmin/external/MvcConfig.java
@@ -14,7 +14,6 @@ public class MvcConfig implements WebMvcConfigurer {
 	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	System.out.println("ADDING");
         registry.addResourceHandler("/" + properties.getBaseUrl() + "/**")
           		.addResourceLocations("classpath:/static/");	
     }
